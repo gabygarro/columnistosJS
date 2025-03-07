ce07e0fc60caae4aa913c272f91372b3721d5bab
gabygarro <ggarroab@gmail.com>
2025-03-07 16:51:46 -0600

    Woots daily stats
    fixes #43

--- utils/date.js
+++ utils/date.js
@@ -1,2 +1,14 @@
 export const getUTCDateForDB = () =>
   new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+export const getYesterdaysDate = () => {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  const day = String(yesterday.getDate()).padStart(2, '0');
+  const month = String(yesterday.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const year = yesterday.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
---
+++ utils/templateTexts.js
@@ -0,0 +1,31 @@
+export const NO_WOMAN = [
+  `Ayer {fecha} en {medio} no hubo ninguna columna de opinión escrita por mujeres.`,
+  `Las {total} columnas de opinión de {medio} en el día de ayer {fecha} fueron escritas por varones.`
+];
+export const ONE_WOMAN = [
+  `De las {total} columnas de opinión que publicaron ayer {fecha} en {medio}, solo una fue escrita por una mujer.`,
+  `En {medio} ayer {fecha} publicaron {total} columnas de opinión y solo una fue escrita por una mujer.`
+];
+export const ONE_MAN = [
+  `De las {total} columnas de opinión que publicaron ayer {fecha} en {medio}, solo una fue escrita por un varón.`,
+  `En {medio} ayer {fecha} publicaron {total} columnas de opinión y solo una fue escrita por un varón.`
+];
+export const SOME_WOMAN = [
+  `De las {total} columnas de opinión que publicaron ayer {fecha} en {medio}, solo {mujeres} fueron escritas por mujeres.`,
+  `En {medio} ayer {fecha} publicaron {total} columnas de opinión y solo {mujeres} fueron escritas por mujeres.`
+];
+export const ALL_WOMAN = [
+  `Ayer {fecha} en {medio} todas las columnas de opinión fueron escritas por mujeres.`,
+  `Las {total} columnas de opinión de {medio}, en el día de ayer {fecha}, fueron escritas por mujeres.`
+];
+export const ONE_WOMAN_COAUTHOR = [
+  `De las {total} columnas de opinión que publicaron ayer {fecha} en {medio}, solo una tuvo al menos una mujer coautora.`,
+  `En {medio} ayer {fecha} publicaron {total} columnas de opinión y solo una fue escrita por al menos una mujer coautora.`
+];
+export const DAILY_REPORT = [
+  'Porcentaje de columnas de opinión publicadas escritas por mujeres en el día de ayer ({fecha}):',
+  'Ayer ({fecha}) el porcentaje de columnistas de opinión mujeres fue:',
+  'De las columnas de opinión publicadas ayer ({fecha}), estas son en porcentaje, las que fueron escritas por mujeres:',
+  'De las columnas de opinión publicadas ayer ({fecha}), estas son en porcentaje, las {escritas} por mujeres:'
+];
+
