d2167b4b5feab64c328fd87c9fd43782f0282de9
gabygarro <ggarroab@gmail.com>
2025-03-06 21:55:01 -0600

    Fix semanario crawler

--- crawlers/cr/semanariouniversidad.js
+++ crawlers/cr/semanariouniversidad.js
@@ -2,18 +2,21 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import { getUTCDateForDB } from '../../utils/date.js';
 
-const BASE_URL = 'https://semanariouniversidad.com';
 const SITE_URL = 'https://semanariouniversidad.com/opinion/';
 
 export default async () => {
-  const { data } = await axios.get(SITE_URL);
+  const { data } = await axios.get(SITE_URL, {
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    },
+  });
   const $ = cheerio.load(data);
   const date_last_seen = getUTCDateForDB();
   const articles = [];
   $('article').each((_, item) => {
     const title = $(item).find('h2').text().trim();
     const author = $(item).find('.entry-meta a').text().trim();
-    const url = `${BASE_URL}${$(item).find('h2 a').attr('href')}`;
+    const url = $(item).find('h2 a').attr('href');
     articles.push({ title, author, url, date_last_seen });
   });
   return { articles, siteName: 'Semanario Universidad', siteUrl: SITE_URL };
