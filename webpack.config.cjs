015e18ca5e9fb8c59ce1b6e010a678793dcad8cb
gabygarro <ggarroab@gmail.com>
2026-09-03 18:50:02 -0600

    Fix bundler

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,20 +3,21 @@ const path = require('path');
 module.exports = {
   target: 'node',
   mode: 'production',
-  entry: {
-    post: './scripts/post.js',
-    crawl: './scripts/crawl.js',
-    sendDms: './scripts/sendDms.js',
+  externalsType: 'module',
+  externals: {
+    mariadb: 'module mariadb',
+    axios: 'module axios',
+    cheerio: 'module cheerio',
+    'wafrn-sdk': 'module wafrn-sdk',
+    dotenv: 'module dotenv',
+    'dotenv/config': 'module dotenv/config',
   },
+  entry: { post: './scripts/post.js', crawl: './scripts/crawl.js', sendDms: './scripts/sendDms.js' },
   output: {
     module: true,
     path: path.resolve(__dirname, 'dist'),
     filename: 'scripts/[name].js',
-    library: {
-      type: 'module'
-    }
+    library: { type: 'module' },
   },
-  experiments: {
-    outputModule: true
-  }
+  experiments: { outputModule: true },
 };
